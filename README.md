a06ca9283c3258ece3265b8966dbf4562e70d9ff
Sergey Volkov <sv.volkoff@gmail.com>
2018-06-19 02:19:19 +0400

    Additional setup, deploy to heroku

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # volkoff.space
+
+[![Build Status](https://travis-ci.org/volkoff-pro/volkoff.space.svg?branch=master)](https://travis-ci.org/volkoff-pro/volkoff.space)
+
+[![Coverage Status](https://coveralls.io/repos/github/volkoff-pro/volkoff.space/badge.svg?branch=master)](https://coveralls.io/github/volkoff-pro/volkoff.space?branch=master)
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 const server = require('./config/server');
 
 const app = express();
 
-require('./routes/routes')(app);
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+// require('./routes/routes')(app);
 
 app.listen(server.PORT);
