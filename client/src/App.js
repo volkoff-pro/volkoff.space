e5947c57a44b8cd37411d3cd04454dee6b6b9f48
sv.volkoff@gmail.com <sv.volkoff@gmail.com>
2018-11-01 01:46:10 +0400

    Clean client. Add Less. Add CSS Modules support.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import block from './app.less';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-);
+const App = () => <div className={block.header}>Main content</div>;
 
 export default App;
